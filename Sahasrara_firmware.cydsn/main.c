fa7e09f10fb633ff3587ecfcda307a8d0bdc9723
Jean-Christophe Sicotte-Brisson <42256489+jcsb1994@users.noreply.github.com>
2021-05-26 10:24:45 -0400

    added 3d models to mount the PCB

--- Sahasrara_firmware.cydsn/main.c
+++ Sahasrara_firmware.cydsn/main.c
@@ -15,11 +15,35 @@ int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    EZI2C_Start();  // Tune capsense sensors with I2C
 
+    EZI2C_EzI2CSetBuffer1(
+                      sizeof(CapSense_dsRam),
+                      sizeof(CapSense_dsRam),
+                      (uint8 *) &CapSense_dsRam 
+                     );
+    
+    CapSense_Start();
+    
+    CapSense_ScanAllWidgets();
+    
+    for(int i = 0; i < 5; i++)
+    {
+        ledPin2_Write(1);
+        ledPin_Write(1);
+        CyDelay(100);
+        ledPin2_Write(0);
+        ledPin_Write(0);
+        CyDelay(100);
+    }
+    
     for(;;)
     {
-        /* Place your application code here. */
+        if(CapSense_IsBusy() == CapSense_NOT_BUSY)
+        {
+            CapSense_RunTuner();
+            CapSense_ScanAllWidgets();
+        }
     }
 }
 
